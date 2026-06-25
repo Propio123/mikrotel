1ce01e93678f414e846da5d10e7d3ff61ad219f6
Propio123 <juanchavez123@hotmail.es>
2026-06-25 11:15:35 -0500

    Estructura inicial del proyecto Mikrotel

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { Navbar } from "./layout/navbar/navbar";
 import { Hero } from "./components/hero/hero";
 import { Footer } from "./layout/footer/footer";
 import { Planes } from "./components/planes/planes";
+import { SocialSidebar } from "./components/social-sidebar/social-sidebar";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBar, Navbar, Hero, Footer, Planes],
+  imports: [RouterOutlet, TopBar, Navbar, Hero, Footer, Planes, SocialSidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
