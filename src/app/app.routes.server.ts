6733a793121e4e900d61d024d930289bff808695
Propio123 <juanchavez123@hotmail.es>
2026-07-07 18:28:54 -0500

    encuesta bien

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
+
+    path: 'encuesta/:isp',
+    renderMode: RenderMode.Client // Cambia esto de Prerender a Client
   }
 ];
