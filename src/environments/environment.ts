17ea1a59bd600b28c4a7ca470891a9d1aee1307f
Propio123 <juanchavez123@hotmail.es>
2026-07-07 16:59:08 -0500

    encuesta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://oadglyrlthxzyadipzar.supabase.co',
-  supabaseKey: 'sb_publishable_wb2mxZ0VOygI6OsWNhNuHg_ru7B72eV'
+  supabaseKey: 'sb_publishable_gHckmpx-T8naZ9BcVOrruA__MA1zYE4'
 };
