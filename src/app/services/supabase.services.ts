84a95b5f665b52cf60b79aa4485aa42c2aabc4cf
Propio123 <juanchavez123@hotmail.es>
2026-07-07 19:20:37 -0500

    encuesta bien

--- src/app/services/supabase.services.ts
+++ src/app/services/supabase.services.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SupabaseService {
   private supabase: SupabaseClient;
 
   constructor() {
-    this.supabase = createClient('supabaseUrl', 'supabaseKey');
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
   }
 
  async guardarEncuesta(datos: any) {
