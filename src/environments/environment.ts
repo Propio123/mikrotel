b8a31d1ae24b47545a806c853097f77a2b8cf67a
Propio123 <juanchavez123@hotmail.es>
2026-07-07 20:24:46 -0500

    encuesta bien

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://placeholder-url.com', // Valor ficticio
+  supabaseKey: 'placeholder-key'             // Valor ficticio
+};
